e99f8a1bf0810b4ed6e5f32799fef778b7d21735
tftg6 <103187047+tftg6@users.noreply.github.com>
2022-05-25 12:03:20 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # configs
-AUUID=24b4b1e1-7a89-45f6-858c-242cf53b5bdb
-CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
+AUUID=772ed5be-eacb-4acf-ace9-8e99736a821a
+CADDYIndexPage=https://github.com/THFBCEZ/thrtger/yhnbfvj/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
